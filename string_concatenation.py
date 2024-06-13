b78aaef257becd3739abc771863ebb36409e60b7
sabir116rus <sabir116rus@gmail.com>
2024-06-13 10:10:03 +0300

    add new file and README

---
+++ string_concatenation.py
@@ -0,0 +1,8 @@
+def concatenate_strings():
+    string1 = input("Введите первую строку: ")
+    string2 = input("Введите вторую строку: ")
+
+    result = string1 + string2
+    print(f"Результат конкатенации: {result}")
+
+concatenate_strings()
